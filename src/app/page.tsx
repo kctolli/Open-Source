0cb6a85fd5f8f650145d583231f4a45dfc652d23
Kyle Tolliver <43971202+kctolli@users.noreply.github.com>
2024-03-02 23:45:56 -0700

    Update page.tsx
    License

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,6 +22,7 @@ export default function Home() {
       <footer className={s.footer}>
         <br /><hr />
         <p>&copy; {(new Date()).getFullYear()} - Kyle Tolliver</p>
+        <p>All rights reserved | Licensed under GPL 3</p>
       </footer>
     </>
   );
