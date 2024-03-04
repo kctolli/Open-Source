944e7f192a7a2cd22f49f88ff20537271af4b140
kctolli <developer@ktolliver.org>
2024-03-03 19:33:08 -0700

    rebuild

--- src/app/build/page.tsx
+++ src/app/build/page.tsx
@@ -1,26 +1,10 @@
 "use client";
-import { Button } from "antd";
-import { RetweetOutlined } from "@ant-design/icons";    
-import axios from 'axios';
+import React from "react";
+import ReBuild from "@kctolli/rebuild-component";
 
 export default function Home() {
     const buildUrl = "https://api.netlify.com/build_hooks/65deba11bb7acd956a44b088";
     const statusUrl = "https://api.netlify.com/api/v1/badges/b979f67f-7023-468d-9442-1a6e76af2807/deploy-status";
 
-    const handleBuild = async () => await axios.post(buildUrl, {});
-
-    return (
-        <main>
-            <iframe 
-                src={statusUrl} 
-                height="20" 
-                width="120" 
-                frameBorder="0"
-            />
-            <br />
-            <Button onClick={handleBuild}>
-                <RetweetOutlined /> Rebuild
-            </Button>
-        </main>
-    );
+    return <ReBuild statusUrl={statusUrl} buildUrl={buildUrl} />;
 }
